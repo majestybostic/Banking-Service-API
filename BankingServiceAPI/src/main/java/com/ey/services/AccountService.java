3a3a6bcbb7a8b3b00036fd8682c4292c7c4e5de0
Majesty Bostic <majesty.bostic@ey.com>
2022-09-21 14:43:29 -0400

    adding accountservice

---
+++ BankingServiceAPI/src/main/java/com/ey/services/AccountService.java
@@ -0,0 +1,15 @@
+package com.ey.services;
+
+import com.ey.models.Account;
+
+import java.util.List;
+
+public interface AccountService {
+
+    public Account addAccount(Account a);
+    public Account getAccount(int id);
+    public Account updateAccount(Account change);
+    public boolean deleteAccount(int id);
+
+    public List<Account> getAccountById(int id);
+}
